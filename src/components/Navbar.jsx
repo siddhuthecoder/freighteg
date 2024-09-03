10ea2eaeccd6abab631e287fb70171258f77af9d
sribabu <sribabumandraju@gmail.com>
2024-09-03 10:53:24 +0530

    new changes to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { VscAccount } from "react-icons/vsc";
-import { MdOutlineAccountBalanceWallet } from "react-icons/md";
+import { mdOutlineAccountBalanceWallet } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { IoIosArrowDown } from "react-icons/io";
 import LeftMenu from "./LeftMenu";
@@ -27,7 +27,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 right-0 bg-gray-300 w-full z-50 text-black shadow-md">
+      <nav className="fixed top-0 left-0 right-0 bg-gray-300 w-full z-50 text-black shadow-lg">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             {/* Left Side: Logo and Navigation Links */}
@@ -40,7 +40,7 @@ const Navbar = () => {
               </div>
 
               {/* Navigation Links */}
-              <div className="hidden md:block text-black">
+              <div className="hidden lg:block text-black">
                 <div className="flex items-center space-x-8 text-black">
                   <NavLink
                     to="/open"
@@ -118,9 +118,9 @@ const Navbar = () => {
             <div className="flex-grow"></div>
 
             {/* Right Side: User Info */}
-            <div className="hidden md:flex items-center space-x-4 absolute top-0 right-0 mt-2 mr-4">
+            <div className="hidden lg:flex items-center space-x-4 absolute top-0 right-0 mt-2 mr-4">
               {/* <div className="bg-white bg-opacity-30 rounded-full flex items-center text-sm focus:outline-none focus:ring-2  transition duration-300 ease-in-out hover:bg-opacity-40 px-4 py-2">
-                <MdOutlineAccountBalanceWallet className="h-6 w-6 text-blue-600 mr-2" />
+                <lgOutlineAccountBalanceWallet className="h-6 w-6 text-blue-600 mr-2" />
                 <div className="flex flex-row">
                   <span className="font-semibold">
                     {getBalance?.data?.bid_balance} Bids||
@@ -144,7 +144,7 @@ const Navbar = () => {
                   />
                 </button>
                 {showDropdown && (
-                  <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition-all duration-300 ease-in-out">
+                  <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-lg shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition-all duration-300 ease-in-out">
                     <Link
                       to="/profile"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition duration-150 ease-in-out"
@@ -163,10 +163,10 @@ const Navbar = () => {
             </div>
 
             {/* Mobile menu button */}
-            <div className="md:hidden">
+            <div className="lg:hidden">
               <button
                 onClick={() => setShowMenu(!showMenu)}
-                className="bg-white bg-opacity-20 inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="bg-white bg-opacity-20 inline-flex items-center justify-center p-2 rounded-lg text-white hover:text-white hover:bg-opacity-30 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               >
                 <span className="sr-only">Open main menu</span>
                 <RxHamburgerMenu className="block h-6 w-6" aria-hidden="true" />
@@ -188,7 +188,7 @@ const NavLink = ({ to, children, isActive }) => {
   return (
     <Link
       to={to}
-      className={`px-3 py-2 rounded-md text-sm font-medium text-black ${
+      className={`px-3 py-2 rounded-lg text-sm font-medium text-black ${
         isActive()
           ? "bg-black bg-opacity-20 text-black"
           : "text-gray-black hover:bg-white hover:bg-opacity-10 hover:text-blue-400"
